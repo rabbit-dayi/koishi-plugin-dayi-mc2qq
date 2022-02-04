a5bc6024586518bb54bf6f7572871ade54593759
dayi <i@dayiyi.top>
2022-02-04 20:03:21 +0800

    fix gb2312 to utf-8

--- plugins/mc1.js
+++ plugins/mc1.js
@@ -1,51 +1,42 @@
 //print("owo")
 console.log("----------------");
 console.log("Hello World");
+console.log("ver Beta 0.0001")
 console.log("----------------");
-
-//yarn add read-last-lines
-//yarn add iconv-lite
-
 
 var bot1
 
-groupid="22133123321" //qq群号
-filepath="Z:/1.12.2_RLCRAFT_1/RLcraft_server_2_b/logs/latest.log" //log文件位置
-con_names=['owo_owo','OUo','ovo','QAQ']  //这些字符串会被接受匹配，并且输出
-con_without=['issued server command','moved too quickly']//包含这些字符串的将不会输出（可能
+groupid="233333333333"
+filepath="Z:/1.12.2_RLCRAFT_1/RLcraft_server_2_b/logs/latest.log"
+con_names=['a','b','c','d','gamemode']
+con_without=['issued server command','moved','PetBlocks','logged','[Rcon]','to player']
 
+
+function is_rubbish_log(text){
+  for(var i=0;i<con_without.length;++i){
+    if(text.toString().indexOf(con_without[i]) != -1){
+      return 1;//辣鸡信息
+    }
+  }
+  return 0;//非辣鸡信息
+}
+
+
+var lastlines=-1
 var fs = require("fs")
-fs.watchFile(filepath, (event, filename) => { 
-
-
+fs.watchFile(filepath,{encoding:'binary'}, (event, filename) => { 
   const readLastLines = require('read-last-lines');
-  readLastLines.read(filepath, 1)
+  readLastLines.read(filepath, 1,"buffer")
       .then((lines) => {
-        
-        //反正，这么一大堆解码，失败了。
-        //呜呜
-
         //对GB2312解码
-        // var iconv = require('iconv-lite'); 
-        // var BufferHelper = require('bufferhelper');
-        // var bufferHelper = new BufferHelper();
-        // bufferHelper.concat(lines);
-        // lines=iconv.decode(bufferHelper.toBuffer(),'GBK')
-        //const { encodeGBK, decodeGBK } = require('gbk-string');
-        //lines=decodeGBK(lines)
         var iconv = require("iconv-lite");
-        var chunks = [];
-
-        var buf=Buffer.from(lines,'utf-8')
-        chunks.push(buf);
-        var lines = iconv.decode(Buffer.concat(chunks), 'GB2312').toString('binary')
-
+        var buf = Buffer.from(lines);
+        lines = iconv.decode(buf, 'GB2312')
         console.log(lines)
-
 
         for(var i=0;i<con_names.length;++i){
           //console.log(con_names[i])
-          if(((c_index=lines.toString().indexOf(con_names[i])) != -1)&& lines.toString().indexOf(con_without[0]) == -1 && lines.toString().indexOf(con_without[1]) ==-1){
+          if(((c_index=lines.toString().indexOf(con_names[i])) != -1)&& !is_rubbish_log(lines)){
             console.log("匹配成功！",con_names[i],lines)
             file_con=lines
             content="[MC]"+file_con.substr(c_index,lines.length-1)
